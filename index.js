15232eb82bb3f750d52c67555befa62fd76f8cfc
Shivam kotak <57216624+shivam12-1@users.noreply.github.com>
2023-11-20 18:49:27 +0530

    first
    first

---
+++ index.js
@@ -0,0 +1,21 @@
+const app = require("./app")
+// const color = require("chalk")
+ const path = require("path");
+ const http = require("http");
+ var server = http.createServer(app);
+
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+const port = 4000
+app.listen(port, () =>
+  console.log(`The server is listning on port `)
+)
+const io = require('socket.io')(server);
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
